ca9c6fa5181cb7501bd1dfe96ad724f0bd214fe1
Daniel <145384176+Daniel16Bit@users.noreply.github.com>
2025-07-21 13:05:24 -0300

    Create aula07.c

---
+++ aula07.c
@@ -0,0 +1,19 @@
+// #7: Type Conversion in C | Implicit and Explicit Type Conversion
+
+#include <stdio.h> 
+
+int main(){
+
+    double a = 5.67;
+    int  b = 9;
+
+    double result = (int)a + b;
+
+    printf("%lf", result);
+
+
+
+
+
+    return 0; 
+}
