1bb3c6336a6e6fbc7da58df079e5562d1dbd64b3
Daniel <145384176+Daniel16Bit@users.noreply.github.com>
2025-07-23 16:01:16 -0300

    !

---
+++ aula13.c
@@ -0,0 +1,25 @@
+// #13: for Loop in C Programming | C Programming for Beginners
+
+#include <stdio.h>
+
+int main(){
+
+
+    /*
+         for (int i = 0; testeExpression; incrementExpression) {
+             code to be executed
+         }
+
+            for (int i = 0; i < 10; i++) { 
+                printf("Hello World!\n");
+            }
+
+    */
+
+    for (int i  = 0; i < 10; i++){
+        printf("Hello World! %d\n", i);
+    }
+
+
+    return 0;
+}
---
+++ aula13b.c
@@ -0,0 +1,19 @@
+// #13: for Loop in C Programming | C Programming for Beginners
+
+#include <stdio.h>
+
+int main() {
+    // Using a for loop to print numbers from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+    // Using a for loop to calculate the sum of numbers from 1 to 10
+    int sum = 0;
+    for (int j = 1; j <= 10; j++) {
+        sum += j;
+    }
+    printf("Sum of numbers from 1 to 10 is: %d\n", sum);
+
+    return 0;
+}
