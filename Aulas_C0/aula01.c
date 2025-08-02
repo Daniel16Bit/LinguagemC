cdcbf3b20dded819c2ac4179563e1d0eb10088de
Daniel <145384176+Daniel16Bit@users.noreply.github.com>
2025-08-02 00:38:40 -0300

    !

---
+++ Aulas_C0/aula01.c
@@ -0,0 +1,10 @@
+// Comece a programar em C em 15 minutos! ⚙️ - https://youtube.com/playlist?list=PLZPZq0r_RZOOzY_vR4zJM32SqsSInGMwe&si=6C8cJRO7E_3fEwtG
+
+
+#include <stdio.h> 
+
+int main(){
+
+    
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,15 @@
+/**
+ * Estudando Linguagem C
+ * 
+ * Este repositório é parte do meu processo de aprendizado.
+ * Estou revisando toda a base da linguagem — variáveis, loops, funções, ponteiros, structs —
+ * com foco em entender profundamente o "ABC" da programação em C e saber aplicá-lo na prática.
+ * 
+ * Todos os códigos aqui são estudos, testes e práticas guiadas por videoaulas, desafios e correções.
+ * O objetivo é evoluir do básico até dominar estruturas mais complexas.
+ *
+ * Qualquer dica ou correção é bem-vinda!
+ * 
+ * Meu instagram é [ @daniel8bit ]
+ * 
+ */
