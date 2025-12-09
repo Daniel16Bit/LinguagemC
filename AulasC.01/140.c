77e35cb0b0efe39c7c545da65f58234a4f2b7fa6
Daniel <145384176+Daniel16Bit@users.noreply.github.com>
2025-12-09 17:33:59 -0300

    !

---
+++ AulasC.01/140.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ | Comando no VisualG      | Comando em C | Biblioteca                      |
+ |=========================|==============|=================================|
+ | escreva / escreval      | printf       | #include <stdio.h>              |
+ ============================================================================
+
+ ============================================================================
+ | Tipo                                  | Placeholder de formatação        |
+ |=======================================|==================================|
+ | int (inteiro 16 bits)                 | %d ou %i                         |
+ | long int (inteiro 32 bits)            | %li                              |
+ | long long int (inteiro 64 bits)       | %lli                             |
+ | float (real precisão simples)         | %f                               |
+ | double (real precisão dupla)          | %lf                              |
+ | char (um único caractere)             | %c                               |
+ | char[ ] (texto)                       | %s                               |
+ ============================================================================
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+
+    return 0;
+}
