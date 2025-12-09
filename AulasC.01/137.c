9989d39c11a07c2d9cd20038b6d397396bfb8af2
Daniel <145384176+Daniel16Bit@users.noreply.github.com>
2025-12-09 00:05:27 -0300

    !

---
+++ AulasC.01/137.c
@@ -0,0 +1,16 @@
+/*
+138. C - Tipos de dados e variáveis em C
+Aula 137 do Professor ~Nelio Alves~
+*/
+
+#include <stdio.h>
+
+int main(){
+
+
+    char Ola[] = "Ola Mundo";
+    printf("Texto: %c\n", Ola);
+
+
+    return 0;
+}
